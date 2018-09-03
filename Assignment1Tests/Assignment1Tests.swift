74dcbeebdc55ed70421b5956bde0f716802514b2
Mark <markrassamni@me.com>
2018-09-03 00:03:05 -0700

    add test cases

--- Assignment1Tests/Assignment1Tests.swift
+++ Assignment1Tests/Assignment1Tests.swift
@@ -11,14 +11,77 @@ import XCTest
 
 class Assignment1Tests: XCTestCase {
     
+    let testElementCount = 100
+    
+    
+    var heap: Heap!
+    
+    var currentID: Int!
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        heap = Heap()
+        currentID = 1
+
+        enqueueRandomStudents(count: testElementCount)
+    }
+    
+
+    
+    func enqueueRandomStudents(count: Int) {
+        for _ in 0..<count {
+            let name = "Name \(currentID)"
+            let redID = "\(currentID)"
+            let email = "\(name)@gmail.com"
+            let unitsTaken = Int(arc4random_uniform(151))
+            let gpa = (Double(arc4random()) / 0xFFFFFFFF) * 4.0
+            let student = Student(name: name, redId: redID, email: email, unitsTaken: unitsTaken, gpa: gpa)
+            currentID! += 1
+            heap.enqueue(student: student)
+        }
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        heap = nil
+        currentID = nil
         super.tearDown()
+        
+    }
+    
+    func testDequeueOrder(){
+        var queue = [Double]()
+        for i in 0..<heap.count {
+            let student = heap.getHighestPriority()
+            queue.append((student?.priority())!)
+            if i > 0 {
+                XCTAssertGreaterThanOrEqual(queue[i-1], queue[i])
+            }
+        }
+    }
+    
+    func testGetHighestElement() {
+        let shouldBeHighest = heap.getHighestPriority()?.priority()
+        var actualHighest: Double = 0.0
+        for i in 0..<heap.count {
+            if heap.queue[i].priority() > actualHighest {
+                actualHighest = heap.queue[i].priority()
+            }
+        }
+        XCTAssertEqual(shouldBeHighest, actualHighest)
+    }
+    
+    func testDequeuePerformance() {
+        self.measure {
+            let _ = heap.dequeue()
+        }
+    }
+    
+    func testEnqueuePerformance() {
+        self.measure {
+            enqueueRandomStudents(count: 1)
+        }
     }
     
     func testExample() {
