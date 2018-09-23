30336948e8c135ef45f05dce5d41001859454ba5
Mark <markrassamni@me.com>
2018-09-23 02:58:02 -0700

    complete remove command

---
+++ Assignment1/Commands/RemoveCommand.swift
@@ -0,0 +1,28 @@
+//
+//  RemoveCommand.swift
+//  Assignment1
+//
+//  Created by Mark Rassamni on 9/23/18.
+//  Copyright © 2018 Mark Rassamni. All rights reserved.
+//
+
+import Foundation
+
+class RemoveCommand<Element, S: Strategy>: Command where Element == S.Element{
+    
+    var priorityQueue: PriorityQueue<Element, S>
+    var element: Element?
+    
+    init(priorityQueue: PriorityQueue<Element, S>) {
+        self.priorityQueue = priorityQueue
+    }
+    
+    func execute() {
+        element = priorityQueue.dequeue()
+    }
+    
+    func undo(){
+        guard let unwrappedElement = element else { return }
+        priorityQueue.enqueue(unwrappedElement)
+    }
+}
