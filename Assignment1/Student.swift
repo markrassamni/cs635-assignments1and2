6c429559aeb3d6320e27e4d7b6dd523e2a94eec3
Mark <markrassamni@me.com>
2018-09-22 23:07:57 -0700

    constant min/max/weights changed to static

--- Assignment1/Student.swift
+++ Assignment1/Student.swift
@@ -14,20 +14,20 @@ struct Student: Equatable {
     private(set) var email: String
     private(set) var unitsTaken: Int
     private(set) var gpa: Double
-    private let unitsWeight = 0.7
-    private let maxUnits = 150
-    private let minUnits = 0
-    private let gpaWeight = 0.3
-    private let maxGPA = 4.0
-    private let minGPA = 0.0
+    static let unitsWeight = 0.7
+    static let maxUnits = 150
+    static let minUnits = 0
+    static let gpaWeight = 0.3
+    static let maxGPA = 4.0
+    static let minGPA = 0.0
     
     // TODO do not hard code in priority function to all students?
     var priority: Double {
-        return Double(unitsTaken) * unitsWeight / Double(maxUnits) + gpa * gpaWeight / maxGPA
+        return Double(unitsTaken) * Student.unitsWeight / Double(Student.maxUnits) + gpa * Student.gpaWeight / Student.maxGPA
     }
     
     init?(name: String, redId: String, email: String, unitsTaken: Int, gpa: Double) {
-        guard unitsTaken >= minUnits, unitsTaken <= maxUnits, gpa >= minGPA, gpa <= maxGPA else {
+        guard unitsTaken >= Student.minUnits, unitsTaken <= Student.maxUnits, gpa >= Student.minGPA, gpa <= Student.maxGPA else {
             return nil
         }
         self.name = name
