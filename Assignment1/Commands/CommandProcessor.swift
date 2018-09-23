d445f32d417d573fb382c8b90a0c6289164185eb
Mark <markrassamni@me.com>
2018-09-23 02:58:37 -0700

    create command processor

---
+++ Assignment1/Commands/CommandProcessor.swift
@@ -0,0 +1,40 @@
+//
+//  CommandProcessor.swift
+//  Assignment1
+//
+//  Created by Mark Rassamni on 9/23/18.
+//  Copyright © 2018 Mark Rassamni. All rights reserved.
+//
+
+import Foundation
+
+class CommandProcessor {
+    
+    var pastStack = [Command]()
+    var futureStack = [Command]()
+    
+    func add(){
+//        let addCommand = AddCommand(priorityQueue: <#T##PriorityQueue<_, _>#>, element: <#T##_#>)
+//        addCommand.execute()
+    }
+    
+    func execute(command: Command){
+        command.execute()
+        pastStack.append(command)
+    }
+    
+    func undo(){
+        guard pastStack.count > 0 else { return }
+        let command = pastStack.removeLast()
+        command.undo()
+        futureStack.append(command)
+    }
+    
+    func redo(){
+        guard futureStack.count > 0 else { return }
+        let command = futureStack.removeLast()
+        command.execute()
+        pastStack.append(command)
+    }
+    
+}
